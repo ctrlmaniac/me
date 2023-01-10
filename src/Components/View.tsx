0286a5ec937a6d7b224552fdf3442e93af6b24f4
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-10 17:30:18 +0100

    add sx

--- src/Components/View.tsx
+++ src/Components/View.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Box } from "@mui/material";
 
 interface Props {
+  sx?: {};
   children: JSX.Element[] | JSX.Element;
 }
 
-const View: React.FC<Props> = ({ children }) => {
+const View: React.FC<Props> = ({ sx, children }) => {
   return (
-    <Box sx={{ height: "100vh", display: "grid", placeItems: "center" }}>
+    <Box sx={{ height: "100vh", display: "grid", placeItems: "center", ...sx }}>
       {children}
     </Box>
   );
