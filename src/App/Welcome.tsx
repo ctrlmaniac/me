0c4c0004c804e81d9a28921ad23e28221a3db9ce
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-21 12:03:28 +0100

    fixes

--- src/App/Welcome.tsx
+++ src/App/Welcome.tsx
@@ -14,15 +14,20 @@ const Welcome: React.FC = () => {
         />
         <Typography variant="h3">Davide Di Criscito</Typography>
         <Typography variant="h4" gutterBottom>
-          Full-Stack web developer. Eager to learn new things, always with one
-          new project in mind, passionate about programming with computers.
+          Full-Stack web developer.
         </Typography>
-        <Chip color="primary" label="JavaScript" />{" "}
-        <Chip color="primary" label="Python" />{" "}
-        <Chip color="primary" label="Java" />{" "}
-        <Link href="#tools" color="secondary">
-          more...
-        </Link>
+        <Typography gutterBottom>
+          Eager to learn new things, always with one new project in mind,
+          passionate about programming with computers.
+        </Typography>
+        <Box mt={2}>
+          <Chip color="primary" label="JavaScript" />{" "}
+          <Chip color="primary" label="Python" />{" "}
+          <Chip color="primary" label="Java" />{" "}
+          <Link href="#tools" color="secondary">
+            more...
+          </Link>
+        </Box>
       </Box>
     </View>
   );
